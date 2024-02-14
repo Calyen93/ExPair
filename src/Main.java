9d4943e9fff99844368c1756f53164e4e64122a2
Your Name <you@example.com>
2024-02-14 10:09:14 +0100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+/*Scrivi un programma Java che contenga due metodi: uno per trovare il valore massimo in un array
+di interi e un altro per trovare la media dei valori in un array di double. Il programma deve creare
+due array, uno di interi e uno di double, e utilizzare i due metodi per trovare il valore massimo e la
+media dei valori in ciascun array.*/
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] arrayInt = {4, 9, 6, 7};
+        double[] arrayInt = {4, 9, 6, 7};
         double[] arrayDouble = {4.5, 3.6, 6.1, 10.4, 12.5};
 
-        int maxInt = maxValue(arrayInt);
+        double maxInt = maxValue(arrayInt);
         double mediaDouble = mediaValue(arrayDouble);
 
 
-        System.out.println("Il Valore massimo di interi è: " + maxInt);
-        System.out.println("La media dei valori dell'array è: " + mediaDouble);
+        System.out.println("Il Valore massimo di interi di arrayInt è: " + maxInt);
+        System.out.println("La media dei valori di arrayInt è: " + mediaDouble);
+        System.out.println ("Il Valore massimo di interi di arrayDouble è " + maxValue(arrayDouble));
+        System.out.println("La media dei valori di arrayDouble è: " + mediaValue(arrayInt));
     }
 
-    public static int maxValue(int[] array) {
-        int max = array[0];
+    public static double maxValue(double[] array) {
+        double max = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
